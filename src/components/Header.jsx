7bbf2da7012f078725671b16b216c8ed43964879
Zachary Kuchar <zacharyk323@live.com>
2023-07-17 19:29:01 -0400

    Added logic for a position and to change the position

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,13 @@ import React, {useContext} from 'react'
 import {ResumeContext} from '../context/app_context'
 
 function Header() {
-    let {name, changeName} = useContext(ResumeContext)
+    let {name, changeName, job, changeJob} = useContext(ResumeContext)
   return (
-    <div>
+    <div className='header'>
       <h1>{name}</h1>
+      <h1>{job}</h1>
       <button onClick={changeName}>Change name</button>
+      <button onClick={changeJob}>Change Position</button>
       <hr />
     </div>
   )
--- src/context/app_context.js
+++ src/context/app_context.js
@@ -5,10 +5,16 @@ export const ResumeContext = createContext();
 const ResumeContextProvider = (props) => {
   const [skills, setSkills] = useState(["React", "javascript"]);
   const [name, setName] = useState("Zack");
+  const [job, setJob] = useState("Software Engineer");
 
   const changeName = () => {
     let n = prompt("Change name to what?");
     setName(n);
+  };
+
+  const changeJob = () => {
+    let j = prompt("Change position to what?");
+    setJob(j);
   };
 
   const addSkill = () => {
@@ -16,7 +22,9 @@ const ResumeContextProvider = (props) => {
     setSkills([...skills, skill]);
   };
   return (
-    <ResumeContext.Provider value={{ skills, addSkill, name, changeName }}>
+    <ResumeContext.Provider
+      value={{ skills, addSkill, name, changeName, job, changeJob }}
+    >
       {props.children}
     </ResumeContext.Provider>
   );
