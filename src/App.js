c5219bed2efd57a61237aa567af33ee0386fca6b
Zachary Kuchar <zacharyk323@live.com>
2023-07-17 19:33:18 -0400

    Added Experience section

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
 import Skills from "./components/Skills";
+import Experience from "./components/Experience";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Skills />
+
       <h1>Add experience</h1>
+      <Experience />
       <h1>Add References</h1>
     </div>
   );
---
+++ src/components/Experience.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Experience() {
+  return (
+    <div>
+      <h1>My Experience:</h1>
+    </div>
+  )
+}
+
+export default Experience
